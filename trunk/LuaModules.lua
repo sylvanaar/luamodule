ee950a6ace7a7f86197ca107765c030dd5bf395c
sylvanaar <sylvanaar@9622ab34-bc22-4ef8-8f63-79f51c7da42a>
2009-01-13 11:33:37 +0000

    annotations and drycode changes to support module(name, ...) where ... are a list of functions to execute on the module. changed package.seeall to a function to fit the api better

--- trunk/LuaModules.lua
+++ trunk/LuaModules.lua
@@ -1,25 +1,40 @@
+-- This is an implemenation of the Lua Module API http://www.lua.org/manual/5.1/manual.html#pdf-module
+
 --@debug@ 
 local dbg = print 
 --@end-debug@
 package = package or {} 
-package.seeall = package.seeall or {__index = _G, __tostring="Global Visibiltity"} 
+
+seeall_mt = {__index = _G}
+package.seeall = package.seeall or function(t) return setmetatable(t, seeall_mt) end 
 package.preload = package.preload or {}
 package.loaders = package.loaders or {}
 package.loaded = package.loaded or {}
 
+
+local function tailExec(t, a, ...)
+    if not a then return t end 
+    return tailExec(a(t), ...)
+end
+
+
 --- Create a Lua Module
---Creates a module. If there is a table in package.loaded[name], this table is the module. Otherwise, if there is a global table t with the given name, this table is the module. Otherwise creates a new table t and sets it as the value of the global name and the value of package.loaded[name]. This function also initializes t._NAME with the given name, t._M with the module (t itself), and t._PACKAGE with the package name (the full module name minus last component; see below). Finally, module sets t as the new environment of the current function and the new value of package.loaded[name], so that require returns t. 
---
---If name is a compound name (that is, one with components separated by dots), module creates (or reuses, if they already exist) tables for each component. For instance, if name is a.b.c, then module stores the module table in field c of field b of global a. 
+--Creates a module. If there is a table in package.loaded[name], this table is the module. 
+--Otherwise, if there is a global table t with the given name, this table is the module. 
+--Otherwise creates a new table t and sets it as the value of the global name and the value of package.loaded[name]. 
+--This function also initializes t._NAME with the given name, t._M with the module (t itself), and t._PACKAGE with the package name (the full module name minus last component). 
+--Finally, module sets t as the new environment of the current function and the new value of package.loaded[name], so that require returns t. 
 --
 --This function can receive optional options after the module name, where each option is a function to be applied over the module
--- @param name The package name. If name is a compound name (that is, one with components separated by dots), module creates (or reuses, if they already exist) tables for each component. For instance, if name is a.b.c, then module stores the module table in field c of field b of global a
--- @param vs The pattern visibility (package.seeall, or nil)
-function module(name, vs) 
+-- @param name The package name. If name is a compound name (that is, one with components separated by dots), module creates (or reuses, if they already exist) tables for each component. 
+-- For instance, if name is a.b.c, then module stores the module table in field c of field b of global a
+-- @param ... usually starts with the pattern visibility package.seeall e.g module("foo", package.seeall)
+function module(name, ...) 
     dbg("\nModule Defined: "..name.."  with visibitlity "..tostring(vs)) 
     local P = package.loaded[name] or _G[name] 
-    if not p then
-        P = setmetatable({}, vs)
+    if not P then
+        P = tailExec({}, ...)
+
         _G[name], package.loaded[name] = P, P
 
         P._NAME = name
